7aacf35d571477dbd5112e7043476bbe488e1d44
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-05-04 08:44:19 +0530

    after errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/product_detail_screen.dart';
 import 'screens/products_overview_screen.dart';
 import 'package:provider/provider.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Color.fromARGB(236, 246, 56, 164),
-          secondary: Color.fromARGB(255, 0, 0, 0),
-          tertiary: Color.fromARGB(255, 14, 247, 255),
-          background: const Color.fromARGB(255, 255, 194, 14),
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Color.fromARGB(236, 246, 56, 164),
+            secondary: Color.fromARGB(255, 0, 0, 0),
+            tertiary: Color.fromARGB(255, 14, 247, 255),
+            background: const Color.fromARGB(255, 255, 194, 14),
+          ),
         ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {},
     );
   }
 }
